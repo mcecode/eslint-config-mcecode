fc65b015db4483538f7c566a4b902803c82f99e2
Matthew Espino <65783406+mcecode@users.noreply.github.com>
2021-08-03 04:31:25 +0800

    Create no-disabled-rules.test.js

---
+++ test/no-disabled-rules.test.js
@@ -0,0 +1,35 @@
+// Destructuring default fixes: https://github.com/avajs/ava/issues/2539.
+const { default: test } = require("ava");
+const { format } = require("util");
+const {
+  BASE_RULES_FILE,
+  PRETTIER_SPECIAL_RULES_FILE
+} = require("../configs/paths");
+
+test("There should be no disabled rules.", (t) => {
+  const rules = {
+    ...require(BASE_RULES_FILE),
+    ...require(PRETTIER_SPECIAL_RULES_FILE)
+  };
+  const rulesKeys = Object.keys(rules);
+
+  const disabledRules = [];
+  rulesKeys.forEach((key) => {
+    const { [key]: ruleValue } = rules;
+
+    if (
+      ruleValue === "off" ||
+      ruleValue === 0 ||
+      (Array.isArray(ruleValue) &&
+        (ruleValue[0] === "off" || ruleValue[0] === 0))
+    ) {
+      disabledRules.push(key);
+    }
+  });
+
+  if (disabledRules.length !== 0) {
+    t.fail(`Disabled rules found: ${format("%O", disabledRules)}`);
+  }
+
+  t.pass();
+});
