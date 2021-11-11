0059dd41736934074e2e324ae533932e6d613c7c
Matthew Espino <65783406+mcecode@users.noreply.github.com>
2021-11-12 00:00:43 +0800

    Update for ESLint v8
    - Add no-unused-private-class-members rule
    - Rearrange base-rules.js based on the updated ESLint rules documentation
    - Remove codeframe as formatter; fallback on stylish as the default formatter
    - Other minor tweaks

--- test/validity.test.js
+++ test/validity.test.js
@@ -1,13 +1,13 @@
 // Destructuring default fixes https://github.com/avajs/ava/issues/2539
 const { default: test } = require("ava");
-const eslint = require("eslint");
+const { ESLint } = require("eslint");
 
 const {
   BASE_RULES_FILE,
   PRETTIER_SPECIAL_RULES_FILE
 } = require("../configs/paths");
 
-const eslintInstance = new eslint.ESLint({
+const eslint = new ESLint({
   useEslintrc: false,
   overrideConfig: {
     env: {
@@ -22,14 +22,14 @@ const eslintInstance = new eslint.ESLint({
 });
 
 test("ESLint rules should be valid.", async (t) => {
-  const [lintResults] = await eslintInstance.lintText("");
+  const [lintResults] = await eslint.lintText("");
 
   t.is(lintResults.messages.length, 0);
   t.is(lintResults.errorCount, 0);
 });
 
 test("ESLint should not find errors.", async (t) => {
-  const [lintResults] = await eslintInstance.lintText(`
+  const [lintResults] = await eslint.lintText(`
     const one = 1;
     console.log(one);
   `);
@@ -40,7 +40,7 @@ test("ESLint should not find errors.", async (t) => {
 });
 
 test("ESLint should find errors.", async (t) => {
-  const [lintResults] = await eslintInstance.lintText("label: void null;");
+  const [lintResults] = await eslint.lintText("label: void null;");
 
   const rulesViolated = lintResults.messages.map((message) => message.ruleId);
 
